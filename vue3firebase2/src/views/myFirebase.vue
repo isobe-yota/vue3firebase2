8c3db29d6e34d62e8eec10f8e55b6be91f31faaf
PC235\isobe.yota <isobe.yota@opusstudio.com>
2023-03-10 14:32:20 +0900

    firestoreに書きこみを行うプログラム vue3firebase2はfirebase.firestore()がうまくいっていないのか動作が成功しない firestoretestではほぼ同じような動きをjavascriptで行っているが成功しています。

---
+++ vue3firebase2/src/views/myFirebase.vue
@@ -0,0 +1,28 @@
+
+<script setup lang="ts">
+ import firebase from "firebase/app";
+import "firebase/compat/firestore";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA7W4_4JY83vpCR1dpcBZimozGxpDqQDXs",
+  authDomain: "vue3firebasetest2.firebaseapp.com",
+  projectId: "vue3firebasetest2",
+  storageBucket: "vue3firebasetest2.appspot.com",
+  messagingSenderId: "625617398171",
+  appId: "1:625617398171:web:a7904dc39bbffd4735193e",
+  measurementId: "G-SV327DCEVN"
+};
+try {
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+  db.collection("user").add({ name: "taro" }).then(docRef => {
+      // success
+  }).catch(error => {
+      console.error(error);
+  });
+} catch (e) {
+  console.error(e);
+}
+            
+            </script> 
